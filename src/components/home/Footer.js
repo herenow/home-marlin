3bb2f54ea701fea69731d343a943b696bdc7933a
Alex Brasileiro <alex.w.brasileiro@gmail.com>
2021-04-30 16:56:01 -0300

    Update with github alerts

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -6,10 +6,10 @@ import stoneLogo from '../../assets/images/starter_member.png';
 export default class Footer extends React.Component {
   renderIcons(forMobile) {
     const mainClass = 'footer__box__icons'
-    const mobileClass = `${ mainClass } ${ mainClass }--mobile`
+    const mobileClass = `${mainClass} ${mainClass}--mobile`
 
     return (
-      <div className={ forMobile ? mobileClass : mainClass }>
+      <div className={forMobile ? mobileClass : mainClass}>
         <i className="mdi mdi-facebook" />
         <i className="mdi mdi-twitter" />
         <i className="mdi mdi-dribbble" />
@@ -24,14 +24,14 @@ export default class Footer extends React.Component {
       <section className="footer">
         <div className="footer__limiter">
           <div className="footer__box">
-            <img className="footer__box__logo" src={logo} alt="marlim logo"/>
-            { this.renderIcons() }
+            <img className="footer__box__logo" src={logo} alt="marlim logo" />
+            {this.renderIcons()}
           </div>
           <div className="footer__stone">
-            <img src={stoneLogo} alt="stone logo"/>
+            <img src={stoneLogo} alt="stone logo" />
           </div>
           <div className="footer__partner">
-            <img src={partnerLogo} alt="partner logo"/>
+            <img src={partnerLogo} alt="partner logo" />
           </div>
           <div className="footer__links">
             <ul>
@@ -39,12 +39,12 @@ export default class Footer extends React.Component {
               <li>Service</li>
               <li>Clients</li>
               <li>Contact us</li>
-              <li>Termos e condições</li>
-              <li>Políticas de privacidade</li>
+              <li><a href="https://docs.google.com/document/u/1/d/e/2PACX-1vSyYThZ7u__rAxFd-bjMRamuFA-4myE3gTZweExO9AngNQTxFt1Wiu8osQwjjyhsIg8jSizSluasuPB/pub" target="_blank">Termos e condições</a></li>
+              <li><a href="https://docs.google.com/document/u/1/d/e/2PACX-1vRG6AhZN04C8N75bD93etgdBDs8mdhSL9BzoopDulWkzjQDCb5AjF-oFTXJx1YoyCrcjhi17HY7ka-y/pub" target="_blank">Política de privacidade</a></li>
             </ul>
           </div>
           <div className="footer__decoration" />
-          { this.renderIcons(true) }
+          {this.renderIcons(true)}
         </div>
       </section>
     );
